351ef8dfae11f84905ae21ac38a9127e5304e5b2
gabriel61 <sogabris@gmail.com>
2023-11-22 20:07:25 -0300

    calculado finish

--- calculadora_simples/scripts.js
+++ calculadora_simples/scripts.js
@@ -26,5 +26,63 @@ const insereNumero = (evento) => {
     atualizaDisplay();
 }
 
+const inserePonto = () => {
+    if (operacaoAtual.indexOf(".") === -1) {
+        operacaoAtual += ".";
+        atualizaDisplay();
+    }
+};
+
+const insereOperador = (evento) => {
+    if (operacaoAtual !== ""){
+        if (!calculando) {
+            if (operador !== null) calcula();
+            valorAnterior = operacaoAtual;
+            operacaoAtual = "";
+        }
+        operador = evento.target.textContent;
+    } 
+};
+
+const calcula = () => {
+    let resultado = null;
+    const operandoAnterior = parseFloat(valorAnterior);
+    const operandoAtual = parseFloat(operacaoAtual);
+
+    switch (operador) {
+        case "+":
+            resultado = operandoAnterior + operandoAtual;
+            break;
+        case "-":
+            resultado = operandoAnterior - operandoAtual;
+            break;
+        case "*" :
+            resultado = operandoAnterior * operandoAtual;
+            break;
+        case "/":
+            if (operandoAtual !== 0) {
+                resultado = operandoAnterior / operandoAtual;
+            } else {
+                alert("Erro: Divisão por zero não é permitida!");
+                return;
+            }
+            break;
+    }
+    operacaoAtual = String(resultado);
+    valorAnterior = operacaoAtual;
+    calculando = true;
+    atualizaDisplay();
+};
+
+
 // EventListeners
+botaoIgual.addEventListener("click", () => {
+    if (operador !== null && operacaoAtual !== "" && !calculando) {
+        calcula();
+        operador = null;
+    }
+});
+botaoPonto.addEventListener("click", inserePonto);
 botoesNumeros.forEach((botao) => botao.addEventListener("click", insereNumero));
+botoesOperadores.forEach((botao) => botao.addEventListener("click", insereOperador));
+
